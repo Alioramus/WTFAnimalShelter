b54d7dd8611bd9f78296b68994fbf9c06e2d5001
j <j.zawadzki@student.uj.edu.pl>
2023-02-07 19:21:11 +0100

    Add basic login window

---
+++ LoginWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WTFAnimalShelter
+{
+    /// <summary>
+    /// Logika interakcji dla klasy LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            var Username = UsernameText.Text;
+            var Password = PasswordText.Password;
+            using(UserDataContext dataContext = new UserDataContext())
+            {
+                bool correctCredentials = dataContext.Users.Any(user => user.Name == Username && user.Password == Password);
+                if (correctCredentials)
+                {
+                    new MainWindow().Show();
+                    Close();
+                } else
+                {
+                    MessageBox.Show("Invalid credentials.");
+                }
+            }
+
+        }
+    }
+}
---
+++ UserDataContext .cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WTFAnimalShelter
+{
+    public class UserDataContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = Data.db");
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
