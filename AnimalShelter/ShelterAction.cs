a042bbbc26b0822422f00ffe95168acbb1bccb96
j <j.zawadzki@student.uj.edu.pl>
2023-02-11 00:08:56 +0100

    Make description optional

--- AnimalShelter/ShelterAction.cs
+++ AnimalShelter/ShelterAction.cs
@@ -9,8 +9,8 @@ public class ShelterAction
     public string Name { get; set; }
     public ShelterActionType Type { get; set; }
     public ShelterActionStatus Status { get; set; }
-    public string Description { get; set; }
-    public User Assignee { get; set; }
+    public string? Description { get; set; }
+    public User? Assignee { get; set; }
     public Animal Animal { get; set; }
         
 }
