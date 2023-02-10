a34795ce204770e4393bdddaf8437d7615490ae2
j <j.zawadzki@student.uj.edu.pl>
2023-02-10 19:18:16 +0100

    Add margins

--- AnimalShelter/Animals/AddAnimalView.xaml.cs
+++ AnimalShelter/Animals/AddAnimalView.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -13,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
 
 namespace AnimalShelter.Animals
 {
@@ -44,5 +46,11 @@ namespace AnimalShelter.Animals
             model.AddAnimal(animal);
             MessageBox.Show("Animal added successfully.");
         }
-    }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+}
 }
