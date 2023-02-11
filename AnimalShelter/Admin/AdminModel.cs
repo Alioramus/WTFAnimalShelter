f65790a181a23611dbb3f889cf7606f75b275413
Szymon <szymo436@gmail.com>
2023-02-11 04:42:49 +0100

    Extend AdminView with list and buttons.

--- AnimalShelter/Admin/AdminModel.cs
+++ AnimalShelter/Admin/AdminModel.cs
@@ -1,10 +1,35 @@
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
 namespace AnimalShelter;
 
-public class AdminModel
+public class AdminModel : INotifyPropertyChanged
 {
     private readonly ShelterContext _context;
+
+    private List<User> _users;
+
+    public List<User> Users
+    {
+        get => _users;
+        set
+        {
+            _users = value;
+            OnPropertyChanged("Users");
+        }
+    }
+
     public AdminModel(ShelterContext context)
     {
         _context = context;
+        Users = _context.Users.Where(user => user != App.CurrentUser).ToList();
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void OnPropertyChanged(string propName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
     }
 }
--- AnimalShelter/Admin/AdminView.xaml.cs
+++ AnimalShelter/Admin/AdminView.xaml.cs
@@ -1,9 +1,21 @@
-﻿namespace AnimalShelter;
+﻿using System.Windows;
+
+namespace AnimalShelter;
 
 public partial class AdminView
 {
     public AdminView()
     {
         InitializeComponent();
+    }
+
+    private void DeleteAccount(object sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void EditAccount(object sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
     }
 }
